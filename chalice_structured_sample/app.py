7c7e4f6cfd7e697ce144470374ae0cd9bfa3d321
Thomas Hughes <thmashu@amazon.com>
2019-06-07 16:13:21 -0400

    Add structured example

---
+++ chalice_structured_sample/app.py
@@ -0,0 +1,82 @@
+try:
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO
+
+from io import BytesIO
+import csv
+import sys, os, base64, datetime, hashlib, hmac
+from chalice import Chalice, Response
+from chalice import NotFoundError, BadRequestError
+import pandas as pd
+import sys, os, base64, datetime, hashlib, hmac
+import requests # pip install requests
+app = Chalice(app_name='CHALICE_PROJECT')
+app.debug = True
+
+try:
+    from urlparse import urlparse, parse_qs
+except ImportError:
+    from urllib.parse import urlparse, parse_qs
+
+import boto3
+sagemaker = boto3.client('sagemaker-runtime')
+
+@app.route('/handle_data', methods=['POST'], content_types=['application/x-www-form-urlencoded'])
+def handle_data():
+    request = app.current_request
+    d = parse_qs(app.current_request.raw_body.decode())
+    # data to csv
+
+    try:
+        my_dict = {k:v[0] for k, v in d.iteritems()}
+    except AttributeError:
+        my_dict = {k:v[0] for k, v in d.items()}
+
+    my_dict['content'] = my_dict.pop('raw_text')
+    f = StringIO()
+    w = csv.DictWriter(f, my_dict.keys())
+    w.writeheader()
+    w.writerow(my_dict)
+
+    content_type = 'text/csv'
+    headers = {'Content-Type':content_type,
+               'Accept': 'Accept'}
+
+    res = sagemaker.invoke_endpoint(
+                    EndpointName='CHALICE_PROJECT',
+                    Body=f.getvalue(),
+                    ContentType='text/csv',
+                    Accept='Accept'
+                )
+    # res.json to dict
+    # format dict to pretty
+
+    result = res['Body']
+
+    result_html = """
+<!DOCTYPE html>
+<html>
+<head>
+        <title>Model Prediction</title>
+<style>
+body{{
+        background-color: white;
+        text-align: center;
+        color: #545b64;
+        font-size: 16px;
+
+}}
+</style>
+</head>
+<body>
+        <center>{0}</center>
+</body>
+</html>
+
+
+    """.format(result.read().decode())
+
+    return Response(body=result_html,
+                    status_code=200,
+                    headers={'Content-Type': 'text/html'})
